b50d58ccb8a7522ba21089552937a75d8fc1c78a
Jindřich Knedla <jindra.knedla@seznam.cz>
2022-11-01 15:59:12 +0100

    initial commit

---
+++ src/components/evaluationGraph.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const evaluationGraph = () => {
+    return (
+        <React.Fragment>
+          <h2>sdfsdfsd</h2>
+        </React.Fragment>
+    );
+}
+
+export default evaluationGraph;
